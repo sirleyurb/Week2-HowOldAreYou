a368f8b33af5881aeccae0bb6d9a5b7712a17c41
Opilane TTHK <opilane@tthk.ee>
2021-09-08 09:43:13 +0300

    Check Year Of Birth

---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //programm küsib kasutaja vanust
+            //programm arvutab kasutaja sünniaastat
+            //programm kuvab kasutaja sünniaastat konsoolis
+
+            Console.WriteLine("Kui vana sa oled?");
+
+            //convert string from ReadLIne() to int
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBirth = 2021 - Age;
+
+            //Console.WriteLine("Olen sündinud aastal" + YearOfBirth + ".");
+            Console.WriteLine($"Olen sündinud aastal {YearOfBirth}."); 
+
+        }
+    }
+}
